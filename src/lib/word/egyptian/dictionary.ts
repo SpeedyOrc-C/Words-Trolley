0fb890519f053b1e7a410c7c3c8f0962955bf759
陈湛明 <chenzhanming04@foxmail.com>
2025-08-31 10:04:13 +0100

    Complete Egyptian demo

---
+++ src/lib/word/egyptian/dictionary.ts
@@ -0,0 +1,186 @@
+import {Phoneme as P} from "$lib/word/egyptian/index"
+import {Glyph as G} from "$lib/word/egyptian/glyph"
+import {g, v, h, type Hieroglyphics} from "$lib/word/egyptian/hieroglyphics"
+
+type HieroglyphicsPrefixTree = {
+	[prefix: string]: [
+		Hieroglyphics[],
+		HieroglyphicsPrefixTree
+	]
+}
+
+const dictionary: HieroglyphicsPrefixTree = {
+	[P.a]: [[
+		g(G.C_a),
+	], {
+	}],
+	[P.i]: [[
+		g(G.C_i),
+	], {
+	}],
+	[P.y]: [[
+		g(G.C_y),
+	], {
+	}],
+	[P.A]: [[
+		g(G.C_A),
+	], {
+		[P.n]: [[], {
+			[P.x]: [[
+				g(G.C_Anx)
+			], {}],
+		}],
+	}],
+	[P.w]: [[
+		g(G.C_w),
+	], {
+	}],
+	[P.b]: [[
+		g(G.C_b),
+	], {
+	}],
+	[P.p]: [[
+		g(G.C_p),
+	], {
+		[P.r]: [[
+			g(G.C_pr),
+			v(g(G.C_pr), g(G.D_identity)),
+		], {
+			[P.t]: [[], {
+				[P.x]: [[], {
+					[P.r]: [[], {
+						[P.w]: [[
+							g(G.L_voice_offering),
+						], {}]
+					}]
+				}]
+			}]
+		}],
+		[P.q]: [[], {
+			[P.r]: [[
+				h(v(h(g(G.C_p), g(G.C_q)), g(G.C_r)), g(G.D_city)),
+			], {}],
+		}],
+	}],
+	[P.f]: [[
+		g(G.C_f),
+	], {
+	}],
+	[P.m]: [[
+		g(G.C_m),
+	], {
+	}],
+	[P.n]: [[
+		g(G.C_n),
+	], {
+		[P.b]: [[
+			g(G.C_nb)
+		], {
+			[P.t]: [[], {
+				[P.a]: [[], {
+					[P.w]: [[], {
+						[P.y]: [[
+							v(g(G.C_nb), g(G.D_earth), g(G.D_earth)),
+						], {}]
+					}]
+				}]
+			}]
+		}],
+		[P.c]: [[], {
+			[P.r]: [[
+				g(G.C_ncr)
+			], {
+				[P.w]: [[
+					h(g(G.C_ncr), g(G.C_ncr), g(G.C_ncr)),
+				], {}],
+			}],
+		}],
+		[P.s]: [[], {
+			[P.w]: [[
+				h(g(G.C_sw), v(g(G.C_t), g(G.C_n))),
+			], {
+				[P.b]: [[], {
+					[P.i]: [[], {
+						[P.t]: [[], {
+							[P.y]: [[
+								h(v(g(G.C_sw), g(G.C_t)), v(g(G.D_bee), g(G.C_t)))
+							], {}]
+						}]
+					}]
+				}]
+			}],
+		}]
+	}],
+	[P.r]: [[
+		g(G.C_r),
+	], {
+		[P.A]: [[
+			g(G.C_rA),
+		], {}],
+	}],
+	[P.h]: [[
+		g(G.C_h),
+	], {}],
+	[P.H]: [[
+		g(G.C_H),
+	], {
+	}],
+	[P.x]: [[
+		g(G.C_x),
+	], {
+		[P.t]: [[
+			v(h(g(G.C_x), g(G.C_t)), g(G.D_papyrus), g(G.D_plural)),
+		], {}]
+	}],
+	[P.C]: [[
+		g(G.C_C),
+	], {
+	}],
+	[P.s]: [[
+		g(G.C_s),
+	], {
+	}],
+	[P.z]: [[
+		g(G.C_z),
+	], {
+	}],
+	[P.S]: [[
+		g(G.C_S),
+	], {
+	}],
+	[P.k]: [[
+		g(G.C_k),
+	], {
+		[P.a]: [[
+			g(G.C_ka),
+			v(g(G.C_ka), g(G.D_identity)),
+			h(g(G.C_ka), g(G.D_identity)),
+		], {}],
+	}],
+	[P.g]: [[
+		g(G.C_g),
+	], {
+	}],
+	[P.q]: [[
+		g(G.C_q),
+	], {
+	}],
+	[P.t]: [[
+		g(G.C_t),
+	], {
+	}],
+	[P.c]: [[
+		g(G.C_c),
+	], {
+	}],
+	[P.d]: [[
+		g(G.C_d),
+	], {
+	}],
+	[P.j]: [[
+		g(G.C_j),
+	], {
+	}],
+}
+
+export default dictionary
--- src/routes/debug/egyptian/+page.svelte
+++ src/routes/debug/egyptian/+page.svelte
@@ -1,38 +1,134 @@
 <script lang="ts">
+	import {Button} from "$lib/components/ui/button"
+	import {Input} from "$lib/components/ui/input/index.js"
+	import type {Phoneme} from "$lib/word/egyptian"
 	import {SetLineHeight} from "$lib/word/egyptian/glyph/height"
 	import G from "$lib/word/egyptian/glyph"
-	import {g, h, v} from "$lib/word/egyptian/hieroglyphics"
+	import {g, h, type Hieroglyphics, v} from "$lib/word/egyptian/hieroglyphics"
 	import Render from "./Render.svelte"
+	import dictionary from "$lib/word/egyptian/dictionary"
 
-	const lineHeight = 96
+	const lineHeight = 64
 	const height = `${lineHeight}px`
-	const gap = `${lineHeight * 0.2}px`
+	const gap = `${lineHeight * 0.1}px`
+
+	let input: string = $state("")
+	let results: Hieroglyphics[] = $state([])
+	let hies = $state<Hieroglyphics[]>([])
 
 	SetLineHeight(lineHeight)
+
+	$effect(() =>
+	{
+		const chars = Array.from(input) as Phoneme[]
+
+		if (chars.length == 0)
+		{
+			results = []
+			return
+		}
+
+		let newResults: Hieroglyphics[] = []
+		let tree = dictionary
+
+		for (let char of chars)
+		{
+			if (! (char in tree))
+			{
+				results = []
+				return
+			}
+
+			newResults = tree[char][0]
+			tree = tree[char][1]
+		}
+
+		results = newResults
+	})
+
+	function onkeydown(e: KeyboardEvent)
+	{
+		if (e.code == "Backspace" && input == "" && hies.length > 0)
+		{
+			e.preventDefault()
+			hies = hies.slice(0, -1)
+			return
+		}
+
+		if (e.code == "Space")
+		{
+			e.preventDefault()
+
+			if (results.length == 0)
+				return
+
+			hies = [...hies, results[0]]
+			input = ""
+			return
+		}
+
+		if (e.code.startsWith("Digit"))
+		{
+			e.preventDefault()
+
+			const i = parseInt(e.code.substring(5)) - 1
+
+			if (i < 0 || i >= results.length)
+				return
+
+			hies = [...hies, results[i]]
+			input = ""
+			return
+		}
+	}
 </script>
 
-<div
-	class="mx-auto p-4 w-fit flex items-center flex-wrap select-none"
-	style:gap style:height
->
-	<div class="text-red-700">
-		<Render hie={h(g(G.A), v(g(G.P), g(G.D)))}/>
-	</div>
-	<div class="text-yellow-700">
-		<Render hie={h(g(G.S), v(g(G.N), h(g(G.B), g(G.I))))}/>
-	</div>
-	<div class="text-green-700">
-		<Render hie={v(h(g(G.P), g(G.X)), g(G.R))}/>
-	</div>
-	<div class="text-blue-700">
-		<Render hie={v(g(G.Z), g(G.K), g(G.R))}/>
-	</div>
-	<Render hie={h(v(g(G.R), g((G.N)), g(G.K)), v(g(G.M), g(G.T)))}/>
-</div>
+<main class="p-4 flex flex-col gap-4">
 
+
+	<div class="flex-2 p-2 rounded-md outline-1">
+		<div class="egyptian egyptian-font" style:gap style:height>
+			{#each hies as hie}
+				<Render {hie}/>
+			{/each}
+		</div>
+	</div>
+
+	<code class="p-2 rounded-md outline-1">
+		{JSON.stringify(hies)}
+	</code>
+
+	<Input
+		bind:value={input} style="font-size: 1rem" type="text" class="font-mono" {onkeydown}
+		placeholder="Transliteration"
+	/>
+
+	<div class="flex gap-4">
+		{#each results as hie, i (hie)}
+			<div class="flex items-center gap-3">
+				<Button variant="outline" onclick={() => {hies = [...hies, hie]; input = ""}}>
+					{i + 1}
+				</Button>
+				<div class="egyptian" style:gap style:height>
+					<div class="egyptian-font">
+						<Render {hie}/>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+
+
+</main>
 
 <style>
-	:global(body) {
+	@reference "tailwindcss";
+
+	.egyptian {
+		@apply w-fit flex items-center flex-wrap select-none;
+	}
+
+	.egyptian-font {
 		font-family: Font_Egyptian, sans-serif;
 	}
 </style>
